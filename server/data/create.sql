598f525b337ef8ac4b45af998820d1ddb16873d3
Uno Karlsson <uno.carlsson@gmail.com>
2018-08-09 18:31:35 +0200

    Refactoring to used pool and async/await to access mysql.

--- server/data/create.sql
+++ server/data/create.sql
@@ -19,79 +19,79 @@ CREATE TABLE `exercise` (
   `name` varchar(80) DEFAULT NULL,
   `imgName` varchar(80) NOT NULL,
   `description` varchar(255) NOT NULL,
-  `bodypartid` int(11) NOT NULL,
+  `bodypartId` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  CONSTRAINT `bodypartid` FOREIGN KEY (`bodypartid`) REFERENCES `bodypart` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `bodypartId` FOREIGN KEY (`bodypartId`) REFERENCES `bodypart` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /* ARM */
-INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartid`) 
+INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartId`) 
 VALUES (1,'Bicep Curl - Dumbbell','bicepCurl-dumbbell.jpg','TODO',1);
 
-INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartid`) 
+INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartId`) 
 VALUES (2,'Tricep Press - Dumbbell','tricepPress-dumbbell.jpg','TODO',1);
 
-INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartid`) 
+INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartId`) 
 VALUES (3,'Narrow Bench Press - Barebell','narrowBenchPress-barebell.png','TODO',1);
 
 /* SHOULDER*/
-INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartid`) 
+INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartId`) 
 VALUES (11,'Shoulder Press - Barebell','shoulderPress-barebell.jpg','TODO',2);
 
-INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartid`) 
+INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartId`) 
 VALUES (12,'Lateral Raises - Dumbbell','lateralRaises-dumbbell.jpg','TODO',2);
 
-INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartid`) 
+INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartId`) 
 VALUES (13,'Shoulder Row - Barebell','shoulderRow-barebell.jpg','TODO',2);
 
 /* Breast */
-INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartid`) 
+INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartId`) 
 VALUES (21,'Bench press - Barebell','benchPress-barebell.jpg','TODO',3);
 
-INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartid`) 
+INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartId`) 
 VALUES (22,'Push Up - Bodyweight','pushUp-bodyweight.jpg','TODO',3);
 
-INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartid`) 
+INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartId`) 
 VALUES (23,'Chest Fly - Dumbbell','chestFly-dumbbell.jpg','TODO',3);
 
 /* Back */
-INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartid`) 
+INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartId`) 
 VALUES (31,'Chins','chins.jpg','TODO',4);
 
-INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartid`) 
+INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartId`) 
 VALUES (32,'Barebell Row','row-barebell.jpg','TODO',4);
 
-INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartid`) 
+INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartId`) 
 VALUES (33,'Pull Down - Machine','pullDown-machine.jpg','TODO',4);
 
 /* Ab */
-INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartid`) 
+INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartId`) 
 VALUES (61,'Crunches - Bodyweight','crunches-bodyweight.jpg','TODO',5);
 
-INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartid`) 
+INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartId`) 
 VALUES (62,'Ab Rotation - Medicin Ball','abRotation-medicinball.jpg','TODO',5);
 
-INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartid`) 
+INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartId`) 
 VALUES (63,'Ab Raises - Machine','abRaises-machine.jpg','TODO',5);
 
 /* Glute */
-INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartid`) 
+INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartId`) 
 VALUES (41,'Back Extention - Bodyweight','backExtention-bodyweight.jpg','TODO',6);
 
-INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartid`) 
+INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartId`) 
 VALUES (42,'Hip Thrust - Barebell','hipThrust-barebell.jpg','TODO',6);
 
-INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartid`) 
+INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartId`) 
 VALUES (43,'Deadlift - Barebell','deadLift-barebell.jpg','TODO',6);
 
 /* Leg */
-INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartid`) 
+INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartId`) 
 VALUES (51,'Squat - Bodyweight','squat-bodyweight.jpg','TODO',7);
 
-INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartid`) 
+INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartId`) 
 VALUES (52,'Lunges - Bodyweight','lunges-bodyweight.jpg','TODO',7);
 
-INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartid`) 
+INSERT INTO `exercise` (`id`,`name`, `imgName`,`description`,`bodypartId`) 
 VALUES (53,'Leg Extentions - Machine','legExtention-machine.jpg','TODO',7);
 
 
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
-const mysql       = require('mysql');
 const cors        = require('cors');
+
+const mysql       = require('mysql');
+const util        = require('util');
 
 const port = 3001;
 
@@ -19,137 +21,125 @@ const myFitnessRemote = ({
     database : 'myfitnessdb'
 });
 
-// let connection = mysql.createConnection(vocabularyRemote);
-let connection = mysql.createConnection(myFitnessRemote);
+let mySqlPool = mysql.createPool(myFitnessRemote);
+mySqlPool.query = util.promisify(mySqlPool.query);
 
 let app = express();
-
 app.use(cors());
 app.use(bodyParser.json());
 
 
 // ========== BODYPARTS ==========
 
-app.get('/bodyparts', (request, response) => {
-    connection.query('SELECT * FROM bodypart',(error,rows) => {
-        if(error) {
-            console.log(error);
-            response.status(400).send();
-        } else {
-            response.send({rows});
-        }
-    });
+app.get('/bodyparts', async (request, response) => {
+    try {
+        const rows = await mySqlPool.query('SELECT * FROM bodypart');
+        response.send({rows});
+    } catch(error) {
+        console.log(error);
+        response.status(400).send();
+    }
 });
 
 
 // ========== EXERCISES ==========
 
-app.get('/exercises', (request, response) => {
-    connection.query('SELECT * FROM exercise', (error, rows) => {
-        if(error) {
-            console.log(error);
-             response.status(400).send();
-        } else {
-            response.send({rows});        
-        }
-    });
+app.get('/exercises', async (request, response) => {
+    try {
+        const rows = await mySqlPool.query('SELECT * FROM exercise');
+        response.send({rows});
+    } catch(error) {
+        console.log(error);
+        response.status(400).send();
+    }
 });
 
 //  Alternative 1
-app.get('/bodyparts/:id/exercises', (request, response) => {
+app.get('/bodyparts/:id/exercises', async (request, response) => {
     let id = request.params.id;
-    connection.query(`SELECT * FROM exercise WHERE bodypartid=${id}`, (error, rows) => {
-        if(error) {
-            console.log(error);
-            response.status(404).send();
-        } else {
-            response.send({rows});        
-        }
-    });
+    try {
+        const rows = await mySqlPool.query(`SELECT * FROM exercise WHERE bodypartId=${id}`);
+        response.send({rows});
+    } catch(error) {
+        console.log(error);
+        response.status(400).send();
+    }
 });
 
 //  Alternative 2
-app.get('/exercises/bodypart/:id', (request, response) => {
+app.get('/exercises/bodypart/:id', async (request, response) => {
     let id = request.params.id;
-    connection.query(`SELECT * FROM exercise WHERE bodypartid=${id}`, (error, rows) => {
-        if(error) {
-             console.log(error);
-             response.status(404).send();
-        } else {
-            response.send({rows});        
-        }
-    });
+    try {
+        const rows = await mySqlPool.query(`SELECT * FROM exercise WHERE bodypartId=${id}`);
+        response.send({rows});
+    } catch(error) {
+        console.log(error);
+        response.status(400).send();
+    }
 });
 
 
 // ========== USERS ==========
 
-app.post('/users',(request,response) => {
+app.post('/users', async (request,response) => {
     const name = request.body.name;
     const userid = request.body.userid;
     const password = request.body.password;
 
-    console.log('name',name);
-    console.log('userid',userid);
-    console.log('password',password);
+    // console.log('name',name);
+    // console.log('userid',userid);
+    // console.log('password',password);
 
     const query = `INSERT INTO user (name,userid,password) VALUES ('${name}','${userid}','${password}')`;
-    console.log('query',query);
+    // console.log('query',query);
 
-    connection.query(query,(error,result) => {
-        if(error) {
-            console.log(error);
-            response.status(400).send();
-        } else {
-            response.send(result);
-        }
-    });
+    try {
+        const rows = await mySqlPool.query(query);
+        response.send({rows});
+    } catch(error) {
+        console.log(error);
+        response.status(400).send();
+    }
 });
 
-app.get('/users',(request,response) => {
-    connection.query('SELECT * FROM user',(error,result) => {
-        if(error) {
-            console.log(error);
-            response.status(400).send();
-        } else {
-            response.send({result});
-        }
-    });
+app.get('/users', async (request,response) => {
+    try {
+        const rows = await mySqlPool.query('SELECT * FROM user');
+        response.send({rows});
+    } catch(error) {
+        console.log(error);
+        response.status(400).send();
+    }
 });
 
-app.get('/users/:id',(request,response) => {
+app.get('/users/:id', async (request,response) => {
     const id = request.params.id;
-
-    connection.query(`SELECT * FROM user WHERE id=${id}`,(error,result) => {
-        if(error) {
-            console.log(error);
-            response.status(400).send();
+    try {
+        const rows = await mySqlPool.query(`SELECT * FROM user WHERE id=${id}`);
+        if(rows.length===0) {
+            response.status(404).send();
         } else {
-            if(result.length===0) {
-                response.status(404).send();
-            } else {
-                response.send({result});
-            }
+            response.send({rows});
         }
-    });
+    } catch(error) {
+        console.log(error);
+        response.status(400).send();
+    }
 });
 
-app.delete('/users/:id',(request,response) => {
+app.delete('/users/:id', async (request,response) => {
     const id = request.params.id;
-
-    connection.query(`DELETE FROM user WHERE id=${id}`,(error,result) => {
-        if(error) {
-            console.log(error);
-            response.status(400).send();
+    try {
+        const rows = await mySqlPool.query(`DELETE FROM user WHERE id=${id}`);
+        if(rows.affectedRows===0) {
+            response.status(404).send();
         } else {
-            console.log
-            if(result.affectedRows===0) {
-                response.status(404).send();
-            } else {
-                response.send({result});
-            }
+            response.send({rows});
         }
-    });
+    } catch(error) {
+        console.log(error);
+        response.status(400).send();
+    }
 });
 
 
@@ -162,52 +152,3 @@ INSERT INTO `myfitnessdb`.`exercise` (`id`, `name`, `bodypartid`) VALUES ('1', '
 INSERT INTO `myfitnessdb`.`exercise` (`id`, `name`, `bodypartid`) VALUES ('2', 'Chins', '2');
 INSERT INTO `myfitnessdb`.`exercise` (`id`, `name`, `bodypartid`) VALUES ('3', 'Squat', '3');
 */ 
-
-/*
-    console.log('Try to connect to database');
-
-    const connection = db.connect((error) => {
-        if(error) {
-            console.log('Unable to connect to mysql',error);
-        } else {
-            console.log('Connected to mysql');
-        }
-    });
-    */
-
-    // Do CRUD operaiotn on mysql
-    /*
-    console.log('Try to query mysql');
-    connection.query('SELECT * FROM word',(error,rows) => {
-        if(error) {
-            console.log('Unable to query mysql');
-            response.status(500).send('Could not retrieve data from mysql');
-        } else {
-            console.log('Able to query mysql');
-            console.log(rows);
-            response.send(rows);
-        }
-
-        // Disconnect from database
-        console.log('Try to disconnect from mysql');
-        connection.end((error) => {
-            if(erro) {
-                console.log('Unable to disconnect from mysql');
-            } else {
-                console.log('Disconnected from mysql')
-            }
-        });
-    });
-    */
-    
-    // Disconnect from database
-    /*
-    console.log('Try to disconnect from mysql');
-    connection.end((error) => {
-        if(erro) {
-            return console.log('Unable to disconnect from mysql');
-        } else {
-            console.log('Dsiconnect from mysql')
-        }
-    });
-    */
